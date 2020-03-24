bcdf19f3d6b789a4b263aef181c79bc7c0da251c
mharris161 <mharris161@gmail.com>
2020-03-24 08:12:57 -0400

    Fix for air influence increasing speed. Rotate double jump functionality.

--- Assets/Scripts/Player/Jumping.cs
+++ Assets/Scripts/Player/Jumping.cs
@@ -9,7 +9,6 @@ public class Jumping : IState
     private float jumpSpeed = 5f;
     private bool _doJump = false;
     private bool _doubleJumpAvailable = true;
-    private float _startingMagnitude = 0f;
     
     private bool JumpDown => PlayerInput.Instance.SpaceDown;
     private bool JumpHeld => PlayerInput.Instance.SpaceHeld;
@@ -24,42 +23,39 @@ public class Jumping : IState
     {
         var stateParamsVelocity = stateParams.Velocity;
 
-        stateParamsVelocity = HandleJumping(stateParamsVelocity);
-        stateParamsVelocity = HandleMovement(stateParamsVelocity);
+        // Gather input and create an input vector from the values
+        float forwardSpeed = PlayerInput.Instance.VerticalRaw;
+        float sideSpeed = PlayerInput.Instance.HorizontalRaw;
+        var inputVelocity = (_player.transform.forward * forwardSpeed) + (_player.transform.right * sideSpeed);
+
+        stateParamsVelocity = HandleJumping(stateParamsVelocity, inputVelocity);
+        stateParamsVelocity = HandleMovement(stateParamsVelocity, inputVelocity);
 
         stateParams.Velocity = stateParamsVelocity;
         return stateParams;
     }
 
-    private Vector3 HandleMovement(Vector3 velocity)
+    private Vector3 HandleMovement(Vector3 velocity, Vector3 inputVelocity)
     {
-        // Get the raw axis input
-        float forwardSpeed = PlayerInput.Instance.VerticalRaw;
-        float sideSpeed = PlayerInput.Instance.HorizontalRaw;
-        
-        // Create of a vector of our input direction relative to the player's forward direction
-        var inputVelocity = (_player.transform.forward * forwardSpeed) + (_player.transform.right * sideSpeed);
-        Debug.DrawRay(_player.transform.position, inputVelocity * 3, Color.blue);
-        
         // Create a vector from our current velocity ignoring the y-axis
         var tempVelocity = new Vector3(velocity.x, 0f, velocity.z);
-        
         // Determine the angle (in degrees) between this direction and our velocity direction
         float angle = Vector3.Angle(tempVelocity, inputVelocity);
-        if (angle > 10)
+        // Move while in the air if it's not directly forward
+        if (angle > 0)
         {
-            float startingMagnitude = velocity.magnitude;
-            velocity += inputVelocity * 0.1f;
-            velocity = Vector3.ClampMagnitude(velocity, startingMagnitude);
+            // Clamp the magnitude so that we're never actually speeding up
+            float startingMagnitude = tempVelocity.magnitude;
+            tempVelocity += inputVelocity * 0.1f;
+            tempVelocity = Vector3.ClampMagnitude(tempVelocity, startingMagnitude);
+            // Apply these updates to our velocity
+            velocity.x = tempVelocity.x;
+            velocity.z = tempVelocity.z;
         }
-        
-        Vector3 debugV = new Vector3(velocity.x, 0f, velocity.z);
-        Debug.Log("Magnitude: " + debugV.magnitude);
-        
         return velocity;
     }
 
-    private Vector3 HandleJumping(Vector3 velocity)
+    private Vector3 HandleJumping(Vector3 velocity, Vector3 inputVelocity)
     {
         // Jump when we first enter the jump state
         if (_doJump)
@@ -76,10 +72,25 @@ public class Jumping : IState
         // Jump if we have a double jump and we hit Jump button
         else if (_doubleJumpAvailable && JumpDown)
         {
-            velocity.y = jumpSpeed;
-            _doubleJumpAvailable = false;
+            velocity = DoubleJump(velocity, inputVelocity);
         }
         return velocity; 
+    }
+
+    private Vector3 DoubleJump(Vector3 velocity, Vector3 inputVelocity)
+    {
+        if (Math.Abs(inputVelocity.magnitude) > 0)
+        {
+            velocity.y = 0;
+            // Find the angle, in radians, between our input direction and current direction
+            float degrees = Vector3.Angle(velocity, inputVelocity);
+            float radians = degrees * Mathf.Deg2Rad;
+            // Rotate the current direction to match our input direction
+            velocity = Vector3.RotateTowards(velocity, inputVelocity, radians, 0.0f);
+        }
+        velocity.y = jumpSpeed;
+        _doubleJumpAvailable = false;
+        return velocity;
     }
 
     public IStateParams OnEnter(IStateParams stateParams)
@@ -88,14 +99,12 @@ public class Jumping : IState
         {
             _doJump = true;
         }
-        _startingMagnitude = stateParams.Velocity.magnitude;
         return stateParams;
     }
 
     public IStateParams OnExit(IStateParams stateParams)
     {
         _doubleJumpAvailable = true;
-        _startingMagnitude = 0f;
         return stateParams;
     }
 
