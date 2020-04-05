cafbfce8b16440c3a3ba99b832d0697104c0a6ff
mharris161 <mharris161@gmail.com>
2020-04-05 10:24:47 -0400

    Full crouching / standing over time implementation. Added player camera caching to the player script. Added some comments

--- Assets/Scripts/Player/Crouching.cs
+++ Assets/Scripts/Player/Crouching.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Crouching : IState
 {
+    public bool IsCrouching { get; private set; } = false;
+    
     private readonly Player _player;
     private CharacterController _characterController;
     private PlayerMovementStateMachine _movementStateMachine;
     private Transform _playerBody;
-
-    public bool IsCrouching { get; private set; } = false;
+    private Transform _playerCamera;
+    
+    private float originalCharacterHeight;
+    private float originalCameraHeight;
 
     private bool _firstframe = false;
     private bool _lowering = false;
@@ -20,12 +24,17 @@ public class Crouching : IState
     {
         _player = player;
         _playerBody = player.PlayerBody;
-        _characterController = player.GetComponent<CharacterController>();
+        _playerCamera = player.PlayerCamera.transform;
         _movementStateMachine = player.GetComponent<PlayerMovementStateMachine>();
+        _characterController = player.GetComponent<CharacterController>();
+        
+        originalCharacterHeight = _characterController.height;
+        originalCameraHeight = _playerCamera.transform.localPosition.y;
     }
 
     public IStateParams Tick(IStateParams stateParams)
     {
+        // Toggling between crouching and standing
         if (!_firstframe && PlayerInput.Instance.CrouchDown)
         {
             _lowering = !_lowering;
@@ -36,6 +45,7 @@ public class Crouching : IState
             _firstframe = false;
         }
         
+        // Perform the actual crouch / stand
         if (_lowering)
         {
             Crouch();
@@ -54,36 +64,73 @@ public class Crouching : IState
     {
         IsCrouching = true;
         var targetBodyScale = 0.5f;
+        var targetCcHeight = originalCharacterHeight / 2;
+        var targetCameraHeight = originalCameraHeight / 2;
+        
         var playerBodyScale = _playerBody.localScale;
-        if (playerBodyScale.y > targetBodyScale && Mathf.Abs(playerBodyScale.y - targetBodyScale) > 0.01f)
-        {
-            playerBodyScale.y = Mathf.Lerp(playerBodyScale.y, targetBodyScale, Time.deltaTime * 8f);
-        }
-        else
-        {
-            playerBodyScale.y = targetBodyScale;
-            _lowering = false;
-        }
+        var ccHeight = _characterController.height;
+        var ccCenter = _characterController.center;
+        var cameraPosition = _playerCamera.transform.localPosition;
+
+        playerBodyScale.y = Lower(playerBodyScale.y, targetBodyScale, Time.deltaTime * 8f);
+        ccHeight = Lower(ccHeight, targetCcHeight, Time.deltaTime * 4f);
+        ccCenter = Vector3.down * (2f - ccHeight) / 2.0f;
+        cameraPosition.y = Lower(cameraPosition.y, targetCameraHeight, Time.deltaTime * 8f);
+        
         _playerBody.localScale = playerBodyScale;
+        _characterController.height = ccHeight;
+        _characterController.center = ccCenter;
+        _playerCamera.transform.localPosition = cameraPosition;
     }
-    
+
     private void Stand()
     {
         IsCrouching = false;
         var targetBodyScale = 1f;
+        var targetCcHeight = originalCharacterHeight;
+        var targetCameraHeight = originalCameraHeight;
+        
         var playerBodyScale = _playerBody.localScale;
-        if (playerBodyScale.y < targetBodyScale && Mathf.Abs(playerBodyScale.y - targetBodyScale) > 0.01f)
+        var ccHeight = _characterController.height;
+        var ccCenter = _characterController.center;
+        var cameraPosition = _playerCamera.transform.localPosition;
+
+        playerBodyScale.y = Raise(playerBodyScale.y, targetBodyScale, Time.deltaTime * 16f);
+        ccHeight = Raise(ccHeight, targetCcHeight, Time.deltaTime * 8f);
+        ccCenter = Vector3.down * (originalCharacterHeight - ccHeight) / 2.0f;
+        cameraPosition.y = Raise(cameraPosition.y, targetCameraHeight, Time.deltaTime * 8f);
+        
+        _playerBody.localScale = playerBodyScale;
+        _characterController.height = ccHeight;
+        _characterController.center = ccCenter;
+        _playerCamera.transform.localPosition = cameraPosition;
+    }
+    
+    private float Lower(float value, float target, float deltaTime)
+    {
+        if (value > target && Mathf.Abs(value - target) > 0.01f)
         {
-            playerBodyScale.y = Mathf.Lerp(playerBodyScale.y, targetBodyScale, Time.deltaTime * 8f);
+            return Mathf.Lerp(value, target, deltaTime);
         }
         else
         {
-            playerBodyScale.y = targetBodyScale;
-            _rising = false;
+            _lowering = false;
+            return target;
         }
-        _playerBody.localScale = playerBodyScale;
     }
 
+    private float Raise(float value, float target, float deltaTime)
+    {
+        if (value < target && Mathf.Abs(value - target) > 0.01f)
+        {
+            return Mathf.Lerp(value, target, deltaTime);
+        }
+        else
+        {
+            _rising = false;
+            return target;
+        }
+    }
     
     public IStateParams OnEnter(IStateParams stateParams)
     {
@@ -97,6 +144,4 @@ public class Crouching : IState
         return stateParams;
     }
 
-    
-    
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,5 +3,8 @@
 public class Player : MonoBehaviour
 {
     [SerializeField] private Transform _playerBody;
+    [SerializeField] private Camera _playerCamera;
+    
     public Transform PlayerBody => _playerBody;
+    public Camera PlayerCamera => _playerCamera;
 }
